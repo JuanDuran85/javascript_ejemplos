8c6f48e75fb0645ebebb0418457dd0c237bb8772
Juan <duran85juan@gmail.com>
2022-02-13 19:48:21 -0300

    patron de composicion

--- ejemplo34/patronesFuncionales.js
+++ ejemplo34/patronesFuncionales.js
@@ -1,1 +1,72 @@
 /* Patrones funcionales:  */
+
+// patron por composicion de funciones
+
+/* Funcion para encontrar niños menores a 12 años y solo mostrar el primero.
+Este codigo sera llevado a uno nuevo para dejar lo imperativo mediante composicion de funciones, permitiendo desacoplar el codigo para que sea reutilizable
+
+const users = [
+    {name: 'John', age: 20, lastname: 'Doe'},
+    {name: 'Anna', age: 12, lastname: 'Smith'},
+    {name: 'Peter', age: 42, lastname: 'Jones'},
+    {name: 'Maria', age: 63, lastname: 'Miller'},
+    {name: 'Susan', age: 4, lastname: 'Brown'},
+];
+
+const getChildren = data => {
+    const children = data.filter(user => user.age < 12);
+    const firstChild = children[0];
+    const child = {
+        fullName: `${firstChild.name} ${firstChild.lastname}`,
+        age: firstChild.age
+    }
+
+    return `${child.fullName} is ${child.age} years old`;
+}
+
+*/
+
+const users = [
+  { name: "John", age: 20, lastname: "Doe" },
+  { name: "Anna", age: 12, lastname: "Smith" },
+  { name: "Peter", age: 42, lastname: "Jones" },
+  { name: "Maria", age: 63, lastname: "Miller" },
+  { name: "Susan", age: 4, lastname: "Brown" },
+];
+
+// funcion para rastreo de pasos
+const trace = x => y => console.log(x, y);
+
+const pipe =
+  (...functions) =>
+  (data) =>
+    functions.reduce((value, funct) => funct(value), data);
+
+// funcion para filtrar datos sin importar la condicion
+const filterData = predicate => data => data.filter(predicate);
+
+// funcion para obtener el primer elemento de un array
+const firstElementInArray = data => data[0];
+
+// funcion para formatear
+const formatChildren = data => ({
+  fullName: `${data.name} ${data.lastname}`,
+  age: data.age,
+});
+
+const childNameLastName = data => `${data.fullName} is ${data.age} years old`;
+
+const getChildren = data =>
+  childNameLastName(
+    formatChildren(firstElementInArray(data.filter((user) => user.age < 12)))
+  );
+
+const getChildrenTwo = pipe(
+  filterData((user) => user.age < 12),
+  firstElementInArray,
+  formatChildren,
+  childNameLastName,
+);
+
+console.info(getChildren(users));
+console.info(getChildrenTwo(users));
