5c88d051a54a3768916266209ecff68aaac0d569
Juan <duran85juan@gmail.com>
2022-06-14 22:49:44 -0400

    Trabajando con arreglos y sus metodos

--- tips_global/arrayMethods.js
+++ tips_global/arrayMethods.js
@@ -93,7 +93,7 @@ console.log('-------------------------------------------------------------------
 // El método slice() devuelve una copia de una parte del array dentro de un nuevo array empezando por inicio hasta fin (fin no incluido). El array original no se modificará. arr.slice([inicio [, fin]])
 // ----------------------------------------------------------------------------------------
 console.log("Slice Method");
-let resultSlice = arrayGlobal.slice(1,3);
+const resultSlice = arrayGlobal.slice(1,3);
 console.log(resultSlice);       
 
 // ----------------------------------------------------------------------------------------
@@ -136,7 +136,7 @@ console.log('-------------------------------------------------------------------
 // El método map() crea un nuevo array con los resultados de la llamada a la función indicada aplicados a cada uno de sus elementos.
 // ----------------------------------------------------------------------------------------
 console.log("map Method");
-let resultMap = arrayGlobal.map(element => element.toLowerCase());
+const resultMap = arrayGlobal.map(element => element.toLowerCase());
 console.log(resultMap);
 
 // ----------------------------------------------------------------------------------------
@@ -146,7 +146,7 @@ console.log('-------------------------------------------------------------------
 // El método filter() crea un nuevo array con todos los elementos que cumplan la condición implementada por la función dada.
 // ----------------------------------------------------------------------------------------
 console.log("filter Method");
-let resultFilter = arrayGlobal.filter(element => element.length > 5);
+const resultFilter = arrayGlobal.filter(element => element.length > 5);
 console.log(resultFilter);
 
 // ----------------------------------------------------------------------------------------
@@ -156,7 +156,7 @@ console.log('-------------------------------------------------------------------
 // El método reduce() ejecuta una función reductora sobre cada elemento de un array, devolviendo como resultado un único valor.
 // ----------------------------------------------------------------------------------------
 console.log("Reduce Method");
-let resultReduce = arrayGlobal.reduce((pre,current)=> `${pre} ${current}`, '-')
+const resultReduce = arrayGlobal.reduce((pre,current)=> `${pre} ${current}`, '-')
 console.log(resultReduce);
 
 // ----------------------------------------------------------------------------------------
@@ -166,7 +166,7 @@ console.log('-------------------------------------------------------------------
 // El método reduceRight() ejecuta una función reductora sobre cada elemento de un array, devolviendo como resultado un único valor. El array se reduce de derecha a izquierda.
 // ----------------------------------------------------------------------------------------
 console.log("reduceRight Method");
-let resultReduceRight = arrayGlobal.reduceRight((pre,current)=> `${pre} ${current}`, '-')
+const resultReduceRight = arrayGlobal.reduceRight((pre,current)=> `${pre} ${current}`, '-')
 console.log(resultReduceRight);
 
 // ----------------------------------------------------------------------------------------
@@ -176,7 +176,7 @@ console.log('-------------------------------------------------------------------
 // El metodo every(): determina si todos los elementos en el array satisfacen una condición.
 // ----------------------------------------------------------------------------------------
 console.log("Every Method");
-let resultEvery = arrayGlobal.every(element => element.length > 4);
+const resultEvery = arrayGlobal.every(element => element.length > 4);
 console.log(resultEvery);
 
 // ----------------------------------------------------------------------------------------
@@ -186,7 +186,7 @@ console.log('-------------------------------------------------------------------
 // El método some() comprueba si al menos un elemento del array cumple con la condición implementada por la función proporcionada.
 // ----------------------------------------------------------------------------------------
 console.log("Some Method");
-let resultSome = arrayGlobal.some(element => element.length > 4);
+const resultSome = arrayGlobal.some(element => element.length > 4);
 console.log(resultSome);
 
 // ----------------------------------------------------------------------------------------
@@ -196,7 +196,7 @@ console.log('-------------------------------------------------------------------
 // El método find() devuelve el valor del primer elemento del array que cumple la función de prueba proporcionada.
 // ----------------------------------------------------------------------------------------
 console.log("Find Method");
-let resultFind = arrayGlobal.find(element => element.length > 4);
+const resultFind = arrayGlobal.find(element => element.length > 4);
 console.log(resultFind);
 
 // ----------------------------------------------------------------------------------------
@@ -206,7 +206,7 @@ console.log('-------------------------------------------------------------------
 // El método findIndex() devuelve el índice del primer elemento de un array que cumpla con la función de prueba proporcionada. En caso contrario devuelve -1.
 // ----------------------------------------------------------------------------------------
 console.log("findIndex Method");
-let resultFindIndex = arrayGlobal.findIndex(element => element.length > 4);
+const resultFindIndex = arrayGlobal.findIndex(element => element.length > 4);
 console.log(resultFindIndex);
 
 // ----------------------------------------------------------------------------------------
@@ -216,7 +216,7 @@ console.log('-------------------------------------------------------------------
 // The fill() method changes all elements in an array to a static value, from a start index (default 0) to an end index (default array.length). It returns the modified array. 
 // ----------------------------------------------------------------------------------------
 console.log("Fill Method");
-let resultFill = arrayGlobal.fill("JS",1,2);
+const resultFill = arrayGlobal.fill("JS",1,2);
 console.log(resultFill);
 
 // ----------------------------------------------------------------------------------------
@@ -226,8 +226,33 @@ console.log('-------------------------------------------------------------------
 // The copyWithin() method shallow copies part of an array to another location in the same array and returns it without modifying its length. 
 // ----------------------------------------------------------------------------------------
 console.log("CopyWithin Method");
-let resultCopyWithin = arrayGlobal.copyWithin(1,2);
+const resultCopyWithin = arrayGlobal.copyWithin(1,2);
 console.log(resultCopyWithin);
 
+// ----------------------------------------------------------------------------------------
+console.log('-----------------------------------------------------------------------------------------');
+
+// ----------------------------------------------------------------------------------------
+// The sort() method sorts the elements of an array in place and returns the sorted array. The default sort order is ascending, built upon converting the elements into strings, then comparing their sequences of UTF-16 code units values. 
+// ----------------------------------------------------------------------------------------
+console.log("Sort Method");
+const arrayToSortOne = [8,4,2,1,0,6,-7,5,3,9]
+let resultSort = arrayToSortOne.sort();
+const arrayToSortTwo = [81,41,21,1,0,64,-7,5,3,9,29,56,6,30,2,7]
+resultSort = arrayToSortTwo.sort();
+console.log(resultSort);
+const arrayToSortThree = ['JavaScript', 'Python', 'C++', 'C', 'PHP','Java', 'Go']
+resultSort = arrayToSortThree.sort();
+console.log(resultSort);
+resultSort = arrayToSortTwo.sort((a,b) => a -b)
+console.log(resultSort);
+const arrayWithObjectToSort = [
+    {name: 'Fernando', age: 20}, 
+    {name: 'Jose', age: 27}, 
+    {name: 'Petra', age: 38},
+    {name: 'Maria', age: 18},
+]
+resultSort = arrayWithObjectToSort.sort((a,b) => a.age - b.age);
+console.log(resultSort);
 // ----------------------------------------------------------------------------------------
 console.log('-----------------------------------------------------------------------------------------');
