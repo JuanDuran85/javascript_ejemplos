9eb6956f24b54f3ba850cdddf4a2d50238563886
Juan <duran85juan@gmail.com>
2022-06-28 17:59:11 -0400

    Trabajando con JS

--- tips_global/stringMethods.js
+++ tips_global/stringMethods.js
@@ -2,13 +2,23 @@
 
     Metodos de String en JavaScript - JavaScript String Methods Cheat Sheet
         - chartAt
+        - concat
+        - indexOf
+        - lastIndexOf
+        - replace
+        - search
+        - slice
+        - substr
+        - toUpperCasa
+        - valueOf
+        - trim
+        - toString
+        - includes
+        - charCodeAt
+        - match
+        - split
+        -toLowerCase
         -
         -
         -
-        -
-        -
-        -
-        -
-        -
-
 */
