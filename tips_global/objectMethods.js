6f7ebb6afaaed74531ee45f9badb599438c85557
Juan <duran85juan@gmail.com>
2022-07-21 10:04:56 -0400

    objects JS

--- tips_global/objectMethods.js
+++ tips_global/objectMethods.js
@@ -42,15 +42,63 @@ const newUser = {
     occupation: 'Engineer',
 };
 
-
 // ----------------------------------------------------------------------------------------
-// The Object.assign() method copies all enumerable own properties from one or more source objects to a target object. It returns the modified target object. 
+// The Object.assign() method copies all enumerable own properties from one or more source objects to a target object. It returns the modified target object. Object.assign(target, source)
 // ----------------------------------------------------------------------------------------
 console.log("assign Method");
+const objectOne = {
+    name: "Juan", 
+    age: 30,
+    occupation: "Developer",
+    country: "Mexico"
+};
+const objectTwo = {
+    name: "Pedro",
+    age: 25,
+    occupation: "Developer"
+};
+console.log({objectOne,objectTwo});
+const resultAssingObject = Object.assign(objectOne, objectTwo);
+console.log({resultAssingObject});
+// Igualmente se puede utilizar destructuracion para obtener los valores de dos objetos en un solo objeto
+const resultDestructuringAssingObject = {...objectOne, ...objectTwo};
+console.log({resultDestructuringAssingObject});
+// ----------------------------------------------------------------------------------------
+console.log('-----------------------------------------------------------------------------------------');
+
+
+// ----------------------------------------------------------------------------------------
+//  The Object.create() method creates a new object, using an existing object as the prototype of the newly created object. 
+// ----------------------------------------------------------------------------------------
+console.log("create Method");
+const personOne = {
+    active: false,
+    showData: function() {
+        console.log(`El nombre es: ${this.nombre} y se encuentra ${this.active ? 'activo' : 'inactivo'}`);
+    }
+};
+
+console.log(personOne);
+
+const newObject = Object.create(personOne);
+newObject.nombre = 'Juan';
+newObject.active = true;
+newObject.showData();
+console.log(newObject);
+// ----------------------------------------------------------------------------------------
+console.log('-----------------------------------------------------------------------------------------');
+
+
+// ----------------------------------------------------------------------------------------
+//  The Object.defineProperties() method defines new or modifies existing properties directly on an object, returning the object. 
+// ----------------------------------------------------------------------------------------
+console.log("defineProperties Method");
+
 
 
 // ----------------------------------------------------------------------------------------
 console.log('-----------------------------------------------------------------------------------------');
+
 
 // ----------------------------------------------------------------------------------------
 // The Object.keys() method returns an array of a given object's own enumerable property names, iterated in the same order that a normal loop would. 
@@ -85,10 +133,7 @@ for (const [key,value] of Object.entries(newUser)) {
 // ----------------------------------------------------------------------------------------
 console.log('-----------------------------------------------------------------------------------------');
 
-// ----------------------------------------------------------------------------------------
-//  
-// ----------------------------------------------------------------------------------------
-console.log(" Method");
+
 
 
 // ----------------------------------------------------------------------------------------
