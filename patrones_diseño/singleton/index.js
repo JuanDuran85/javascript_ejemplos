64574909b3eaa1c227a4e06f878f93aff7671037
Juan <duran85juan@gmail.com>
2023-05-12 23:45:54 -0400

    singleton pattern

--- patrones_diseño/singleton/index.js
+++ patrones_diseño/singleton/index.js
@@ -1,8 +1,9 @@
 /**
  *
- * Patron de Diseño - Singleton
+ * Patron de Diseño - Singleton (Singleton Pattern)
  *
  * Que solamente existe un objeto de la clase
+ * Singleton design pattern exposes a single instance that can be used by multiple components
  * It is a pattern that restricts the class to create only one instance.
  * They can be accessed globally and acts as a single access point for accessing the global state.
  */
@@ -63,3 +64,35 @@ const weekDaysObject = new WeekDaysClass('en');
 const newWeekDaysObject = new WeekDaysClass('es');
 console.log(weekDaysObject.getDays());
 console.log(newWeekDaysObject.getDays());
+
+// ---------------------------------------------------------------------------------
+
+let instance;
+let globalState = {
+    color: ""
+};
+
+class StateUtility {
+    constructor(){
+        if(instance) throw new Error("New instance cannot be created!!");
+        instance = this;
+    }
+
+    getPropertyByName(propertyName){
+        return globalState[propertyName];
+    }
+
+    setPropertyValue(propertyName, propertyValue){
+        globalState[propertyName] = propertyValue;
+    }
+}
+
+
+const stateUtilityInstance = Object.freeze(new StateUtility());
+console.log(stateUtilityInstance);
+stateUtilityInstance.setPropertyValue("leng", "JS");
+stateUtilityInstance.setPropertyValue("color", "Blue");
+console.log(stateUtilityInstance.getPropertyByName("leng"));
+console.log(stateUtilityInstance.getPropertyByName("color"));
+
+
