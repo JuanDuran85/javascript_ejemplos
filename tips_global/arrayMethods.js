3aa3876b9e567b69cdc18586b05d12291230d3fb
JuanDuran85 <duran85juan@gmail.com>
2024-11-26 11:27:04 -0300

    add new array method

--- tips_global/arrayMethods.js
+++ tips_global/arrayMethods.js
@@ -722,3 +722,8 @@ console.debug(
 //-----------------------------------------------------------------------------------
 // The with() method of Array instances is the copying version of using the bracket notation to change the value of a given index. It returns a new array with the element at the given index replaced with the given value.
 //-----------------------------------------------------------------------------------
+
+
+
+
+
